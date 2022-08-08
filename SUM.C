be562a7315be624dc9c75aa1b0e4c3b5d94bcc13
Kavvya332 <79555216+Kavvya332@users.noreply.github.com>
2022-08-08 16:03:38 +0530

    Add files via upload

---
+++ SUM.C
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    int sum=0;
+    cout<<"\n enter the number ";
+    cin>>x>>y;
+    sum+=x+y;
+    cout<<"\n the sum of two numbers are \t"<<sum;
+
+    return 0;
+}
